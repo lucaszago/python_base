3565cf2774484935e613d71f12479c1a6eff4063
Lucas Zago <lukaszago@hotmail.com>
2023-12-02 14:06:45 -0300

    Implement Hello World

--- hello.py
+++ hello.py
@@ -1,5 +1,49 @@
 #!/usr/bin/env python 
 
-# Comentário
-print("Hello World")
-iprint(56 + 7)
+"""
+Hello World Multi Language 
+According to the language configured on env 
+Usage: 
+    Have LANG variable configured accordingly ex:
+    export LANG = pt_BR 
+Execution: 
+    python3 hello.py
+"""
+
+
+__version__ = "0.0.1"
+__author__ = "Lucas"
+__license__ = "unlicense"
+
+import os
+
+
+
+current_language = os.getenv("LANG","en_US")[:5]
+
+
+
+
+
+msg = "Hello World!"
+
+if current_language == "pt_BR":
+    msg = "Olá Mundo"
+
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde!"
+
+    
+
+
+
+
+print(msg)
+
+
+
